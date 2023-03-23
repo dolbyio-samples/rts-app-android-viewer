0177fa7e1b39eececce58e15ac4d4e754da943d4
Liubov Danilina <liubov.danilina@dolby.com>
2023-03-23 17:54:55 +1100

    - Focus behaviour fixed for DetailInputScreen.kt - Keyboard is hidden by default for TextInput.kt - Keyboard is hidden by default for MainActivity

--- app/src/main/java/io/dolby/rtsviewer/ui/detailInput/DetailInputScreen.kt
+++ app/src/main/java/io/dolby/rtsviewer/ui/detailInput/DetailInputScreen.kt
@@ -56,8 +56,6 @@ import io.dolby.rtsviewer.uikit.button.StyledButton
 import io.dolby.rtsviewer.uikit.input.TextInput
 import io.dolby.rtsviewer.uikit.theme.fontColor
 
-@OptIn(ExperimentalComposeUiApi::class)
-@Suppress("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun DetailInputScreen(
     onPlayClick: (StreamingData) -> Unit,
@@ -106,9 +104,10 @@ fun DetailInputScreen(
             DolbyCopyrightFooterView()
         },
         modifier = modifier
-    ) {
+    ) { paddingValues ->
         DolbyBackgroundBox(
             modifier = modifier
+                .padding(paddingValues)
                 .semantics { contentDescription = screenName }
         ) {
             val localFocusManager = LocalFocusManager.current
@@ -128,16 +127,6 @@ fun DetailInputScreen(
                     .clip(MaterialTheme.shapes.large)
                     .padding(horizontal = 55.dp)
                     .padding(vertical = 16.dp)
-                    .onKeyEvent {
-                        if (it.type == KeyEventType.KeyUp && it.key == Key.DirectionDown) {
-                            localFocusManager.moveFocus(FocusDirection.Down)
-                            return@onKeyEvent true
-                        } else if (it.type == KeyEventType.KeyUp && it.key == Key.DirectionUp) {
-                            localFocusManager.moveFocus(FocusDirection.Up)
-                            return@onKeyEvent true
-                        }
-                        return@onKeyEvent true
-                    }
             ) {
                 Text(
                     stringResource(id = R.string.stream_detail_header),
@@ -177,7 +166,7 @@ fun DetailInputScreen(
                     keyboardActions = KeyboardActions(
                         onNext = { localFocusManager.moveFocus(FocusDirection.Down) }
                     ),
-                    modifier = modifier.focusRequester(focusRequester)
+                    modifier = Modifier.focusRequester(focusRequester)
                 )
 
                 Spacer(modifier = modifier.height(8.dp))
@@ -188,9 +177,9 @@ fun DetailInputScreen(
                     onValueChange = {
                         accountId = it
                     },
-                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
                     keyboardActions = KeyboardActions(
-                        onDone = { localFocusManager.clearFocus() }
+                        onNext = { localFocusManager.moveFocus(FocusDirection.Down) }
                     )
                 )
 
