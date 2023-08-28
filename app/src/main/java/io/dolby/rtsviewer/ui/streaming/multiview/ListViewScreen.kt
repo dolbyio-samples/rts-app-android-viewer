644300fe20d451aa3ca09ae0fff7c298eccba84b
Liubov Danilina <liubov.danilina@dolby.com>
2023-08-28 11:43:14 +1000

    ListViewScreen.kt for portrait / landscape

--- app/src/main/java/io/dolby/rtsviewer/ui/streaming/multiview/ListViewScreen.kt
+++ app/src/main/java/io/dolby/rtsviewer/ui/streaming/multiview/ListViewScreen.kt
@@ -1,16 +1,19 @@
 package io.dolby.rtsviewer.ui.streaming.multiview
 
-import android.util.Log
+import android.content.res.Configuration
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -18,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
@@ -27,6 +31,7 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.millicast.VideoRenderer
+import io.dolby.rtscomponentkit.data.MultiStreamingData
 import io.dolby.rtscomponentkit.ui.DolbyBackgroundBox
 import io.dolby.rtscomponentkit.ui.LiveIndicator
 import io.dolby.rtscomponentkit.ui.TopAppBar
@@ -74,84 +79,196 @@ fun ListViewScreen(
                     CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
                 }
                 uiState.videoTracks.isNotEmpty() -> {
-                    Log.d(
-                        "===>",
-                        "video size: ${uiState.videoTracks.size}, audio size: ${uiState.audioTracks.size}"
-                    )
-                    Box(
-                        modifier = Modifier.align(Alignment.Center)
-                    ) {
-                        SetupVolumeControlAudioStream()
-                        Column {
-                            Box(modifier = Modifier.clickable {
-                                onMainClick(uiState.videoTracks.firstOrNull()?.id)
-                            }) {
-                                AndroidView(
-                                    factory = { context ->
-                                        val view = VideoRenderer(context)
-                                        view.setZOrderOnTop(true)
-                                        view.setZOrderMediaOverlay(true)
-                                        view
-                                    },
-                                    update = { view ->
-                                        view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT)
-                                        val videoTrack = uiState.selectedVideoTrackId?.let { selectedVideoTrackId ->
-                                            uiState.videoTracks.firstOrNull { it.sourceId == selectedVideoTrackId }
-                                        } ?: uiState.videoTracks.firstOrNull()
-                                        videoTrack?.videoTrack?.setRenderer(
-                                            view
-                                        )
-                                    }
-                                )
-                                Text(
-                                    text = uiState.selectedVideoTrackId ?: "Main",
-                                    modifier = Modifier.align(
-                                        Alignment.BottomStart
-                                    )
-                                )
-                            }
-                            val otherTracks = uiState.videoTracks.filter { it.sourceId != uiState.selectedVideoTrackId }
-                            Log.d("===>", "other tracks: $otherTracks, selectedSourceId = ${uiState.selectedVideoTrackId}")
-                            LazyVerticalGrid(
-                                columns = GridCells.Fixed(count = 2),
-                                modifier = Modifier
-                                    .fillMaxHeight()
-                                    .padding(top = 5.dp),
-                                verticalArrangement = Arrangement.spacedBy(5.dp),
-                                horizontalArrangement = Arrangement.spacedBy(5.dp),
-                            ) {
-                                items(items = otherTracks) { videoTrack ->
-                                    Box {
-                                        AndroidView(
-                                            modifier = Modifier
-                                                .aspectRatio(16F / 9)
-                                                .clickable {
-                                                    viewModel.selectVideoTrack(videoTrack.sourceId)
-                                                },
-                                            factory = { context -> VideoRenderer(context) },
-                                            update = { view ->
-                                                view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT)
-                                                view.release()
-                                                videoTrack.videoTrack.setRenderer(view)
-                                            }
-                                        )
-                                        Text(
-                                            text = videoTrack.sourceId ?: "Main",
-                                            modifier = Modifier.align(
-                                                Alignment.BottomStart
-                                            )
-                                        )
-                                    }
-                                }
-                            }
-                        }
-                        LiveIndicator(
-                            modifier = Modifier.align(Alignment.TopStart),
-                            on = uiState.videoTracks.isNotEmpty() || uiState.audioTracks.isNotEmpty()
+                    val configuration = LocalConfiguration.current
+                    val onOtherClick = { videoTrack: MultiStreamingData.Video ->
+                        viewModel.selectVideoTrack(videoTrack.sourceId)
+                    }
+
+                    if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                        horizontalEndListView(
+                            modifier = Modifier,
+                            uiState,
+                            onMainClick,
+                            onOtherClick
                         )
+                    } else {
+                        verticalTopListView(
+                            modifier = Modifier.align(Alignment.Center),
+                                uiState,
+                                onMainClick,
+                                onOtherClick
+                            )
                     }
                 }
             }
         }
     }
+}
+
+@Composable
+fun horizontalEndListView(
+    modifier: Modifier,
+    uiState: MultiStreamingUiState,
+    onMainClick: (String?) -> Unit,
+    onOtherClick: (MultiStreamingData.Video) -> Unit
+) {
+    Box(
+        modifier = modifier
+    ) {
+        SetupVolumeControlAudioStream()
+        Row {
+            Box(modifier = Modifier.clickable {
+                onMainClick(uiState.videoTracks.firstOrNull()?.id)
+            }) {
+                AndroidView(
+                    factory = { context ->
+                        val view = VideoRenderer(context)
+                        view.setZOrderOnTop(true)
+                        view.setZOrderMediaOverlay(true)
+                        view
+                    },
+                    update = { view ->
+                        view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT)
+                        val videoTrack =
+                            uiState.selectedVideoTrackId?.let { selectedVideoTrackId ->
+                                uiState.videoTracks.firstOrNull { it.sourceId == selectedVideoTrackId }
+                            } ?: uiState.videoTracks.firstOrNull()
+                        videoTrack?.videoTrack?.setRenderer(
+                            view
+                        )
+                    }
+                )
+                Text(
+                    text = uiState.selectedVideoTrackId ?: "Main",
+                    modifier = Modifier.align(
+                        Alignment.BottomStart
+                    )
+                )
+            }
+            val otherTracks =
+                uiState.videoTracks.filter { it.sourceId != uiState.selectedVideoTrackId }
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(top = 5.dp),
+                verticalArrangement = Arrangement.spacedBy(5.dp)
+            ) {
+                items(otherTracks) { videoTrack ->
+                    Box {
+                        AndroidView(
+                            modifier = Modifier
+                                .aspectRatio(16F / 9)
+                                .clickable { onOtherClick.invoke(videoTrack) },
+                            factory = { context -> VideoRenderer(context) },
+                            update = { view ->
+                                view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT)
+                                view.release()
+                                videoTrack.videoTrack.setRenderer(view)
+                            }
+                        )
+                        Text(
+                            text = videoTrack.sourceId ?: "Main",
+                            modifier = Modifier.align(
+                                Alignment.BottomStart
+                            )
+                        )
+                    }
+                }
+            }
+        }
+        liveIndicatorComponent(
+            modifier = Modifier.align(Alignment.TopStart),
+            on = uiState.videoTracks.isNotEmpty() || uiState.audioTracks.isNotEmpty()
+        )
+    }
+}
+
+@Composable
+fun verticalTopListView(
+    modifier: Modifier,
+    uiState: MultiStreamingUiState,
+    onMainClick: (String?) -> Unit,
+    onOtherClick: (MultiStreamingData.Video) -> Unit
+) {
+    Box(
+        modifier = modifier
+    ) {
+        SetupVolumeControlAudioStream()
+        Column {
+            Box(modifier = Modifier.clickable {
+                onMainClick(uiState.videoTracks.firstOrNull()?.id)
+            }) {
+                AndroidView(
+                    factory = { context ->
+                        val view = VideoRenderer(context)
+                        view.setZOrderOnTop(true)
+                        view.setZOrderMediaOverlay(true)
+                        view
+                    },
+                    update = { view ->
+                        view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT)
+                        val videoTrack =
+                            uiState.selectedVideoTrackId?.let { selectedVideoTrackId ->
+                                uiState.videoTracks.firstOrNull { it.sourceId == selectedVideoTrackId }
+                            } ?: uiState.videoTracks.firstOrNull()
+                        videoTrack?.videoTrack?.setRenderer(
+                            view
+                        )
+                    }
+                )
+                Text(
+                    text = uiState.selectedVideoTrackId ?: "Main",
+                    modifier = Modifier.align(
+                        Alignment.BottomStart
+                    )
+                )
+            }
+            val otherTracks =
+                uiState.videoTracks.filter { it.sourceId != uiState.selectedVideoTrackId }
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(count = 2),
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(top = 5.dp),
+                verticalArrangement = Arrangement.spacedBy(5.dp),
+                horizontalArrangement = Arrangement.spacedBy(5.dp),
+            ) {
+                items(items = otherTracks) { videoTrack ->
+                    Box {
+                        AndroidView(
+                            modifier = Modifier
+                                .aspectRatio(16F / 9)
+                                .clickable {
+
+                                },
+                            factory = { context -> VideoRenderer(context) },
+                            update = { view ->
+                                view.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT)
+                                view.release()
+                                videoTrack.videoTrack.setRenderer(view)
+                            }
+                        )
+                        Text(
+                            text = videoTrack.sourceId ?: "Main",
+                            modifier = Modifier.align(
+                                Alignment.BottomStart
+                            )
+                        )
+                    }
+                }
+            }
+        }
+        liveIndicatorComponent(
+            modifier = Modifier.align(Alignment.TopStart),
+            on = uiState.videoTracks.isNotEmpty() || uiState.audioTracks.isNotEmpty()
+        )
+    }
+}
+
+@Composable
+fun liveIndicatorComponent(modifier: Modifier, on: Boolean) {
+    LiveIndicator(
+        modifier = modifier,
+        on = on
+    )
 }
