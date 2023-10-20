445934b7d8a1330e7d16df0e07a25b8cce974fbe
Liubov Danilina <liubov.danilina@dolby.com>
2023-10-20 16:34:20 +1100

    UI adjustments for Connection options

--- interactiveplayer/src/main/java/io/dolby/interactiveplayer/detailInput/DetailInputScreen.kt
+++ interactiveplayer/src/main/java/io/dolby/interactiveplayer/detailInput/DetailInputScreen.kt
@@ -1,25 +1,40 @@
 package io.dolby.interactiveplayer.detailInput
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ExposedDropdownMenuBox
+import androidx.compose.material.ExposedDropdownMenuDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Slider
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,8 +47,10 @@ import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import io.dolby.interactiveplayer.R
 import io.dolby.interactiveplayer.alert.ClearStreamConfirmationAlert
 import io.dolby.interactiveplayer.alert.DetailInputValidationAlert
@@ -45,6 +62,7 @@ import io.dolby.interactiveplayer.utils.horizontalPaddingDp
 import io.dolby.rtsviewer.uikit.button.ButtonType
 import io.dolby.rtsviewer.uikit.button.StyledButton
 import io.dolby.rtsviewer.uikit.theme.fontColor
+import io.dolby.rtsviewer.uikit.utils.buttonHeight
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -184,6 +202,7 @@ fun DetailInputScreen(
                 Spacer(modifier = modifier.height(12.dp))
 
                 if (showDebugOptions.value) {
+                    ConnectionOptions(viewModel)
                 }
 
                 StyledButton(
@@ -212,8 +231,10 @@ fun DetailInputScreen(
 
                 Spacer(modifier = modifier.height(12.dp))
                 val demoConnectionOptions = if (showDebugOptions.value) {
-                    val recentDemoStream = recentStreams.value.recentStreams.firstOrNull { it.accountID == DEMO_ACCOUNT_ID && it.streamName == DEMO_STREAM_NAME }
-                    val connectOptions = recentDemoStream?.let { ConnectOptions.from(it) } ?: ConnectOptions()
+                    val recentDemoStream =
+                        recentStreams.value.recentStreams.firstOrNull { it.accountID == DEMO_ACCOUNT_ID && it.streamName == DEMO_STREAM_NAME }
+                    val connectOptions =
+                        recentDemoStream?.let { ConnectOptions.from(it) } ?: ConnectOptions()
                     connectionOptionsText(connectOptions)
                 } else null
                 StyledButton(
@@ -229,6 +250,152 @@ fun DetailInputScreen(
                     capitalize = false,
                     endIcon = painterResource(id = io.dolby.uikit.R.drawable.ic_play_outlined)
                 )
+            }
+        }
+    }
+}
+
+@Composable
+fun ConnectionOptions(viewModel: DetailInputViewModel) {
+    val selectedConnectOptions = viewModel.selectedConnectionOptions.collectAsState()
+    var isExpanded by rememberSaveable { mutableStateOf(false) }
+    Card(
+        modifier = Modifier.clickable { isExpanded = !isExpanded }
+    ) {
+        Column {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .buttonHeight(ButtonType.PRIMARY)
+                    .padding(8.dp)
+            ) {
+                Text(
+                    stringResource(id = R.string.connection_options_more_stream_config_title),
+                    textAlign = TextAlign.Start
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Image(
+                    painter = painterResource(
+                        id = if (isExpanded) {
+                            io.dolby.uikit.R.drawable.ic_arrow_down_24
+                        } else io.dolby.uikit.R.drawable.ic_arrow_right_24
+                    ),
+                    contentDescription = stringResource(
+                        id = R.string.connection_options_more_stream_config_title
+                    )
+                )
+            }
+
+            AnimatedVisibility(visible = isExpanded) {
+                Column(modifier = Modifier.padding(8.dp)) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(stringResource(id = R.string.stream_connection_options_dev_server_title))
+                        Spacer(modifier = Modifier.weight(1.0f))
+                        Switch(
+                            checked = selectedConnectOptions.value.useDevEnv,
+                            onCheckedChange = { viewModel.updateUseDevEnv(it) }
+                        )
+                    }
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(stringResource(id = R.string.stream_connection_options_force_playout_delay_title))
+                        Spacer(modifier = Modifier.weight(1.0f))
+                        Switch(
+                            checked = selectedConnectOptions.value.forcePlayOutDelay,
+                            onCheckedChange = { viewModel.updateForcePlayOutDelay(it) }
+                        )
+                    }
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(stringResource(id = R.string.stream_connection_options_disable_audio_title))
+                        Spacer(modifier = Modifier.weight(1.0f))
+                        Switch(
+                            checked = selectedConnectOptions.value.disableAudio,
+                            onCheckedChange = { viewModel.updateDisableAudio(it) }
+                        )
+                    }
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(stringResource(id = R.string.stream_connection_options_save_logs_title))
+                        Spacer(modifier = Modifier.weight(1.0f))
+                        Switch(
+                            checked = selectedConnectOptions.value.rtcLogs,
+                            onCheckedChange = { viewModel.updateRtcLogs(it) }
+                        )
+                    }
+
+                    var sliderValue by remember { mutableFloatStateOf(0f) }
+                    Text(
+                        text = stringResource(id = R.string.connection_options_jitter_buffer_delay_title) +
+                            " - ${selectedConnectOptions.value.videoJitterMinimumDelayMs}ms"
+                    )
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(
+                            "0",
+                            overflow = TextOverflow.Visible,
+                            maxLines = 1
+                        )
+
+                        Spacer(modifier = Modifier.width(5.dp))
+
+                        Slider(
+                            value = sliderValue,
+                            valueRange = 0f..2000f,
+                            steps = (2000 / 50) - 1,
+                            onValueChange = {
+                                sliderValue = it
+                                viewModel.updateJitterBufferMinimumDelay(it.toInt())
+                            },
+                            modifier = Modifier.weight(1f)
+                        )
+
+                        Spacer(modifier = Modifier.width(5.dp))
+
+                        Text(
+                            "2sec",
+                            overflow = TextOverflow.Visible,
+                            maxLines = 1
+                        )
+                    }
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(stringResource(id = R.string.stream_connection_options_primary_video_quality_title))
+                        Spacer(modifier = Modifier.weight(1.0f))
+                        QualityLabel(viewModel = viewModel)
+                    }
+                    Spacer(modifier = Modifier.height(12.dp))
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun QualityLabel(viewModel: DetailInputViewModel) {
+    val selectedConnectionOptions = viewModel.selectedConnectionOptions.collectAsState()
+    val showVideoQualitySelection = viewModel.showVideoQualityState.collectAsStateWithLifecycle()
+    Row {
+        ExposedDropdownMenuBox(
+            expanded = showVideoQualitySelection.value,
+            onExpandedChange = { viewModel.showPrimaryVideoQualitySelection(it) }
+        ) {
+            TextField(
+                value = selectedConnectionOptions.value.primaryVideoQuality.name,
+                onValueChange = {},
+                readOnly = true,
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = showVideoQualitySelection.value) }
+            )
+            ExposedDropdownMenu(
+                expanded = showVideoQualitySelection.value,
+                onDismissRequest = { viewModel.showPrimaryVideoQualitySelection(false) }
+            ) {
+                viewModel.videoQualities().forEach { item ->
+                    DropdownMenuItem(onClick = {
+                        viewModel.updatePrimaryVideoQuality(item)
+                        viewModel.showPrimaryVideoQualitySelection(false)
+                    }) {
+                        Text(text = item.name)
+                    }
+                }
             }
         }
     }
